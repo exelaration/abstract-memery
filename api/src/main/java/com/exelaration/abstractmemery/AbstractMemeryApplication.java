e525d71c51732bddd381198ac0557848ab4674e9
Jordan Phillips <jordan@jphillips.lj4hkbnoztgubj23h51urnq0qc.bx.internal.cloudapp.net>
2020-02-05 18:23:21 +0000

    bootstrapped the backend spring boot api

---
+++ api/src/main/java/com/exelaration/abstractmemery/AbstractMemeryApplication.java
@@ -0,0 +1,13 @@
+package com.exelaration.abstractmemery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractMemeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractMemeryApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/exelaration/abstractmemery/AbstractMemeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exelaration.abstractmemery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractMemeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
