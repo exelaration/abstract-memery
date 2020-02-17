26bc610e0f522c0b3c90cca941ce36beda16d195
sulaip1 <sulaip1@vt.edu>
2020-02-17 15:27:07 +0000

    removed bean for CORS

--- api/src/main/java/com/exelaration/abstractmemery/controllers/UploadController.java
+++ api/src/main/java/com/exelaration/abstractmemery/controllers/UploadController.java
@@ -1,27 +1,25 @@
 package com.exelaration.abstractmemery.controllers;
 
-import java.util.List;
-
 import com.exelaration.abstractmemery.domains.Caption;
-// import com.exelaration.abstractmemery.repositories.MemeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(value = "http://localhost:3000")
+@RequestMapping("/upload")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UploadController {
 
-    @GetMapping(value = "/upload", produces = "application/json")
+    @GetMapping(value = "/", produces = "application/json")
     @ResponseBody
     public ResponseEntity<?> showText(@RequestBody Caption caption){
         Logger logger = LoggerFactory.getLogger(UploadController.class);
@@ -29,7 +27,7 @@ public class UploadController {
         return new ResponseEntity<>(caption, HttpStatus.OK);
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/")
     public ResponseEntity newCaption(@RequestBody Caption caption) {
         Logger logger = LoggerFactory.getLogger(UploadController.class);
         logger.info("\nPost Request\nTop Text: " + caption.getTopText() + "\nBottom Text: " + caption.getBottomText());
