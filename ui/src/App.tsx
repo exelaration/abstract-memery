2d569e086373ef10fb57cdcdcabe8262a3358b2f
sulaip1 <sulaip1@vt.edu>
2020-02-21 18:15:41 +0000

    Add meme preview

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TextUpload from './TextUpload';
 import ImageUpload from './ImageUpload';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TextUpload />
-        <ImageUpload/>
-      </header>
-    </div>
-  );
+type Props = {
+}
+
+type State = {
+  topText: String,
+  bottomText: String,
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {topText: '', bottomText: ''}
+  }
+
+  setTopText = (text: String) => {
+    this.setState({topText: text});
+  }
+
+  setBottomText = (text: String) => {
+    this.setState({bottomText: text});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="inputPrompt">Create Your Meme</h1>
+          <div className="memeCreation">
+            <ImageUpload topText={this.state.topText} bottomText={this.state.bottomText}/>
+            <TextUpload sendTopText={this.setTopText.bind(this)} sendBottomText={this.setBottomText.bind(this)}/>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
