5f518257625847b5ba5f7ff9285855d33ee68832
Katheryn Osmond <katheryn.osmond@exelaration.com>
2020-02-24 16:54:52 +0000

    make app run without compile errors but data does not save to database

--- api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
+++ api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
@@ -1,5 +1,11 @@
 package com.exelaration.abstractmemery.domains;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import lombok.Data;
 
