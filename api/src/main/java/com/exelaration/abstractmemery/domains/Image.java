07de7aa512075458e7de78ddc529b737c76c2087
Katheryn Osmond <katheryn.osmond@exelaration.com>
2020-02-26 17:50:05 +0000

    allow tags to be saved to the database and link to a specific image

--- api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
+++ api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
@@ -1,10 +1,15 @@
 package com.exelaration.abstractmemery.domains;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
