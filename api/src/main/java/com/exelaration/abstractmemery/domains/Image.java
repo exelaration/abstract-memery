c659f58be1da2908e80d15dc51d556940c7f5703
Katheryn Osmond <katheryn.osmond@exelaration.com>
2020-02-26 17:00:13 +0000

    allow user to successfully save image data to the database

--- api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
+++ api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
@@ -3,6 +3,7 @@ package com.exelaration.abstractmemery.domains;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
