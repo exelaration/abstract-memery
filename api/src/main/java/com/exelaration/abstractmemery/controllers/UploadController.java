908e3fa9bbafac28205df0563db7fdee75ccd5f4
sulaip1 <sulaip1@vt.edu>
2020-02-28 18:33:34 +0000

    separated out image upload into differe services

--- api/src/main/java/com/exelaration/abstractmemery/controllers/UploadController.java
+++ api/src/main/java/com/exelaration/abstractmemery/controllers/UploadController.java
@@ -1,15 +1,8 @@
 package com.exelaration.abstractmemery.controllers;
 
-<<<<<<< HEAD
 import com.exelaration.abstractmemery.domains.Image;
 import com.exelaration.abstractmemery.services.ImageService;
-=======
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import org.apache.tomcat.util.codec.binary.Base64;
->>>>>>> make app run without compile errors but data does not save to database
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
