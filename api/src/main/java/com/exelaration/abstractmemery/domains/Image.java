e7911d6d7b3df5df46703c532e5c738b3656ecbc
Katheryn Osmond <katheryn.osmond@exelaration.com>
2020-03-04 16:23:26 +0000

    format correctly and add the meme domain

--- api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
+++ api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
@@ -1,14 +1,9 @@
 package com.exelaration.abstractmemery.domains;
 
-import java.util.Set;
-
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import lombok.Data;
@@ -30,8 +25,7 @@ public class Image {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
-  @Transient 
-  private String fileData;
+  @Transient private String fileData;
 
   private String fileName;
 
