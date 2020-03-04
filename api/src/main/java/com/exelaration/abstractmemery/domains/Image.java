f631d743c0240400a147967589322f798bfc282e
Katheryn Osmond <katheryn.osmond@exelaration.com>
2020-03-04 16:19:31 +0000

    make migrations rely on flyway instead of hibernate auto-creation

--- api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
+++ api/src/main/java/com/exelaration/abstractmemery/domains/Image.java
@@ -1,6 +1,8 @@
 package com.exelaration.abstractmemery.domains;
 
 import java.util.Set;
+
+import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.SequenceGenerator;
@@ -13,6 +15,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import lombok.Data;
@@ -26,7 +30,8 @@ public class Image {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
-  @Transient private String fileData;
+  @Transient 
+  private String fileData;
 
   private String fileName;
 
