d27a0fcdf837ddf48d672ea30b522b545b262b6e
Jordan Phillips <pjordan1@vt.edu>
2020-03-18 17:55:24 +0000

    Added Mockito to the gradle dependencies

--- api/src/test/java/com/exelaration/abstractmemery/services/ImageServiceTest.java
+++ api/src/test/java/com/exelaration/abstractmemery/services/ImageServiceTest.java
@@ -2,37 +2,50 @@ package com.exelaration.abstractmemery.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+
 import com.exelaration.abstractmemery.domains.Image;
+import com.exelaration.abstractmemery.services.implementations.FileStorageServiceImpl;
+import com.exelaration.abstractmemery.services.implementations.ImageServiceImpl;
+import com.exelaration.abstractmemery.services.implementations.MetadataServiceImpl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 
 @ExtendWith(MockitoExtension.class)
 public class ImageServiceTest {
 
-    @Mock 
+    @Mock
     private FileStorageService fileStorageService;
 
-    @Mock 
+    @Mock
     private MetadataService metadataService;
-    
+
     @InjectMocks
-    private ImageService imageService;
+    private ImageServiceImpl imageService;
 
     @Test
-    public void testSaveImageSuccessfully() {
+    public void testSaveImageSuccessfully() throws IOException {
         //look into moving this stuff into a set up method with @before annotation
-        MockMultipartFile mockMultipartFile = new MockMultipartFile("user-image", "test-image",
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("user-image", "test-image.png",
             "image/png", "imageData".getBytes());
 
-        Image mockImage = new Image();
-        mockImage.setFileName("user-image");
-        mockImage.setFileData("imageData");
-        mockImage.setFileLocation("fileLocation");
+        Image expectedImage = new Image();
+        expectedImage.setFileName("test-image.png");
+        expectedImage.setFileData("imageData");
+        expectedImage.setFileLocation("fileLocation");
 
-        Mockito.when(metadataService.save(mockImage)).thenReturn(mockImage);
-        Mockito.when(fileStorageService.save("user-image", mockMultipartFile.getInputStream())).thenReturn("fileLocation");
+        Mockito.when(metadataService.save(expectedImage)).thenReturn(expectedImage);
+        Mockito.when(fileStorageService.save("test-image.png", mockMultipartFile.getInputStream())).thenReturn("fileLocation");
 
-        Image testImage = imageService.save(mockMultipartFile);
+        Image actualImage = imageService.save(mockMultipartFile);
 
-        assertThat(testImage).isSameAs(mockImage);
+        assertEquals(expectedImage, actualImage);
 
     }
 }
