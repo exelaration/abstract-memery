3ee6f0bb674e31bdc35718ac51bf7e228e9a242a
Matt McHugh <matt.mchugh@exelaration.com>
2020-04-03 12:28:23 -0400

    Add CI GitHub workflow

--- api/src/test/java/com/exelaration/abstractmemery/controllers/MemeControllerTest.java
+++ api/src/test/java/com/exelaration/abstractmemery/controllers/MemeControllerTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 import com.exelaration.abstractmemery.domains.Meme;
 import com.exelaration.abstractmemery.services.implementations.MemeServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,40 +23,46 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = MemeController.class)
 public class MemeControllerTest {
-    private MockMvc mockMvc;
-    @Autowired MemeController memeController;
-    @MockBean private MemeServiceImpl memeService;
+  private MockMvc mockMvc;
+  @Autowired MemeController memeController;
+  @MockBean private MemeServiceImpl memeService;
 
-    @BeforeEach
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(memeController).build();
-      }
-    
-    @Test
-    public void MemeControllerTest_WhenMemePosts_ExpectStatus200() throws Exception {
-        ResultMatcher ok = MockMvcResultMatchers.status().isOk();
-        String url = "/meme/";
-        
-        Meme testMeme = new Meme();
-    
-        when(memeService.save(any())).thenReturn(testMeme);
-    
-        mockMvc.perform(MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON)
-        .content(new ObjectMapper().writeValueAsString(testMeme))).andExpect(ok);
-      }
+  @BeforeEach
+  public void setUp() {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(memeController).build();
+  }
 
-    @Test
-    public void MemeControllerTest_WhenNullMemePosts_ExpectStatus400() throws Exception {
-        ResultMatcher badRequest = MockMvcResultMatchers.status().isBadRequest();
-        String url = "/meme/";
-        
-        Meme testMeme = null;
-    
-        when(memeService.save(any())).thenReturn(testMeme);
-    
-        mockMvc.perform(MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON)
-        .content(new ObjectMapper().writeValueAsString(testMeme))).andExpect(badRequest);
-      }
-    
+  @Test
+  public void MemeControllerTest_WhenMemePosts_ExpectStatus200() throws Exception {
+    ResultMatcher ok = MockMvcResultMatchers.status().isOk();
+    String url = "/meme/";
 
+    Meme testMeme = new Meme();
+
+    when(memeService.save(any())).thenReturn(testMeme);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(testMeme)))
+        .andExpect(ok);
+  }
+
+  @Test
+  public void MemeControllerTest_WhenNullMemePosts_ExpectStatus400() throws Exception {
+    ResultMatcher badRequest = MockMvcResultMatchers.status().isBadRequest();
+    String url = "/meme/";
+
+    Meme testMeme = null;
+
+    when(memeService.save(any())).thenReturn(testMeme);
+
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(testMeme)))
+        .andExpect(badRequest);
+  }
 }
