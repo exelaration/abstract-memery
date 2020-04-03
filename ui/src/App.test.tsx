6bc9fd989a97d19c2a78ed3524b65b6b9b4c1b52
Matt McHugh <matt.mchugh@exelaration.com>
2020-04-03 12:28:23 -0400

    Add CI GitHub workflow

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders page title', () => {
   const { getByText } = render(<App />);
   const headerElement = getByText(/create your meme/i);
   expect(headerElement).toBeInTheDocument();
