2b4c53a8d2ff63d4b95a2714ac90c367ef39333c
Jordan Phillips <pjordan1@vt.edu>
2020-04-06 17:56:03 +0000

    add user sign up page

--- ui/src/CreateMeme.tsx
+++ ui/src/CreateMeme.tsx
@@ -3,6 +3,7 @@ import { AppDispatchContext, AppDispatchReducer, initialState } from './reducers
 import './CreateMeme.css';
 import MemeUpload from './MemeUpload';
 import ImageUpload from './ImageUpload';
+import Button from 'react-bootstrap/Button';
 
 function CreateMeme(){
     const [appState, dispatch] = useReducer(AppDispatchReducer, initialState);
@@ -11,6 +12,10 @@ function CreateMeme(){
     return(
         <div className="Create">
             <header className="Create-header">
+                <div className='row'>
+                <Button type='submit' href='/sign-up' variant='primary'>Sign Up</Button>
+                <Button type='submit' href='/login' variant='primary'>Login</Button>
+                </div>
                 <h1 className="inputPrompt">Create Your Meme</h1>
                 <div className="memeCreation">
                     <AppDispatchContext.Provider value={value}>
