6d25e398c9f75b863b3d4c98a276c39334966ff9
Jordan Phillips <pjordan1@vt.edu>
2020-04-06 17:56:03 +0000

    add user sign up page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Upload from './CreateMeme';
 import RouterError from './RouterError';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CreateMeme from './CreateMeme';
+import SignUp from './SignUp';
 
 function App() {
 
   return (
     <Router>
       <Switch>
-        <Route path = "/" component = {Upload} exact />
+        <Route path = "/" component = {CreateMeme} exact />
+        <Route path = "/sign-up" component = {SignUp} exact/>
         <Route component = {RouterError} />
       </Switch>
     </Router>
