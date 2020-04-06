e021cd47623456e62c570d715cb030a6447e600b
Jordan Phillips <pjordan1@vt.edu>
2020-04-06 17:56:03 +0000

    create Login page and format

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,6 +3,7 @@ import {Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import RouterError from './RouterError';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateMeme from './CreateMeme';
+import Login from './Login';
 import SignUp from './SignUp';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path = "/" component = {CreateMeme} exact />
         <Route path = "/sign-up" component = {SignUp} exact/>
+        <Route path = "/login" component = {Login} exact/>
         <Route component = {RouterError} />
       </Switch>
     </Router>
