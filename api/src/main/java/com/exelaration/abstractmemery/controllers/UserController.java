06577cc202f27a8e1c28ac2bc7c51080c151392e
Jordan Phillips <pjordan1@vt.edu>
2020-05-26 15:08:47 +0000

    add search for users

--- api/src/main/java/com/exelaration/abstractmemery/controllers/UserController.java
+++ api/src/main/java/com/exelaration/abstractmemery/controllers/UserController.java
@@ -1,18 +1,23 @@
 package com.exelaration.abstractmemery.controllers;
 
 import com.exelaration.abstractmemery.domains.ApplicationUser;
+import com.exelaration.abstractmemery.services.UserService;
 import com.exelaration.abstractmemery.services.implementations.UserDetailsServiceImpl;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired private UserService userService;
   @Autowired private UserDetailsServiceImpl userDetailsService;
 
   @PostMapping("/sign-up")
@@ -31,5 +36,10 @@ public class UserController {
       return "User has been updated";
     }
     return "User already exists";
+  }
+
+  @GetMapping(value = "/", params = "text")
+  public ArrayList<String> getUsersForSearch(@RequestParam String text) throws Exception {
+    return userService.getUsersWithText(text);
   }
 }
---
+++ api/src/main/java/com/exelaration/abstractmemery/projections/UserProjection.java
@@ -0,0 +1,7 @@
+package com.exelaration.abstractmemery.projections;
+
+public interface UserProjection {
+  String getUsername();
+
+  void setUsername(String username);
+}
