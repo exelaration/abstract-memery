a626a4a93a6d9ea43ff1e98e3bed1c24729c2212
sulaip1 <sulaip1@vt.edu>
2020-05-28 16:52:15 +0000

    e2e test fails on failed to launch the browser process

--- ui/src/tests/integration/index.test.ts
+++ ui/src/tests/integration/index.test.ts
@@ -1,33 +1,38 @@
 import 'expect-puppeteer';
-// const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
 
-// beforeAll(async() => {
-//   browser = await puppeteer.launch({
-//     args: [
-//       // Required for Docker version of Puppeteer
-//       '--no-sandbox',
-//       '--disable-setuid-sandbox',
-//       // This will write shared memory files into /tmp instead of /dev/shm,
-//       // because Docker’s default for /dev/shm is 64MB
-//       '--disable-dev-shm-usage'
-//     ]
-//   })
+beforeAll(async() => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: "/usr/bin/chromium-browser",
+    args: [
+      // Required for Docker version of Puppeteer
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      // This will write shared memory files into /tmp instead of /dev/shm,
+      // because Docker’s default for /dev/shm is 64MB
+      '--disable-dev-shm-usage'
+    ]
+  })
 
-//   const browserVersion = await browser.version()
-//   console.log(`Started ${browserVersion}`)
-// })
+  const browserVersion = await browser.version()
+  console.log(`Started ${browserVersion}`)
+})
 
-// beforeEach(async() => {
-//   page = await browser.newPage()
-// })
+beforeEach(async() => {
+  const page = await browser.newPage()
+})
 
-// afterEach(async() => {
-//   await page.close()
-// })
+afterEach(async() => {
+  await page.close()
+})
 
-// afterAll(async() => {
-//   await browser.close()
-// })
+afterAll(async() => {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  await browser.close()
+})
 
 describe('App', () => {
   beforeAll(async() => {
